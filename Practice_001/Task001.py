1c8ea28bcff30e9a43069196145869d9bc02022b
Smusss <lilly_dallas@bk.ru>
2022-12-21 16:14:57 +0300

    Add Practice Task001

--- Practice_001/Task001.py
+++ Practice_001/Task001.py
@@ -1,20 +1,7 @@
-# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-# Пример:
-# - 6 -> да
-# - 7 -> да
-# - 1 -> нет
+number1 = int(input('Введите первое число: '))
+number2 = int(input('Введите второе число: '))
 
-def determine_day (number):
-    try:
-        number = int(number)
-        if number == 6 or number == 7:
-            print('YES! It is holiday, buddy!')
-        elif number < 1 or number > 7:
-            print('Uncorrect input')
-        else:
-            print('NO, it is weekday')
-    except:
-        print('Uncorrect input (symbol)')
-
-number_of_day = input('Input number of a day (from 1 to 7): ')
-determine_day(number_of_day)
+if number2 == number1**2 or number1 == number2**2:
+    print ('Yes')
+else:
+    print ('No')
