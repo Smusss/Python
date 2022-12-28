90a6340903a0fbf27ff082a8cdbbea2d2778973c
Smusss <lilly_dallas@bk.ru>
2022-12-28 17:53:14 +0300

    Add  Homework_002 Task001_HW

--- Homework_002/Task001_HW.py
+++ Homework_002/Task001_HW.py
@@ -2,3 +2,14 @@
 # Пример:
 # 6782 -> 23
 # 0,56 -> 11
+
+number = input('Input number: ')
+print(f'{number} ->', end=' ')
+number = number.split(',')
+#print(number)
+sum = 0
+for element in number:
+    for num in element:
+        sum = sum + int(num)
+print(sum, end='. ')
+print(f'Sum of elements = {sum}')
