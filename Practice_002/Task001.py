f291bbdb4898fed32264ceca4a2344ccd4c3bbf0
Smusss <lilly_dallas@bk.ru>
2022-12-28 17:33:34 +0300

    modified Practice_002/Task001.py

--- Practice_002/Task001.py
+++ Practice_002/Task001.py
@@ -6,3 +6,15 @@ my_list = [1]
 for i in range(1, number):
     my_list.append(my_list[i-1] * -3)
 print(*my_list)
+
+number = int(input('Введите число N: '))
+print(f"N = {number}:", end=" ")
+for i in range(number):
+    print((-3)**i, end=", ")
+
+number = int(input('Введите число N: '))
+my_list = []
+for i in range(number):
+    my_list.append((-3)**i)
+print(f"N = {number}:", end=" ")
+print(*my_list, sep=", ")
