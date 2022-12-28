298e985092d314bd714aa85dff60b3cd78d1240c
Smusss <lilly_dallas@bk.ru>
2022-12-28 18:20:56 +0300

    Add  Homework_002 Task002_HW

--- Homework_002/Task002_HW.py
+++ Homework_002/Task002_HW.py
@@ -2,3 +2,17 @@
 # Пример:
 # Для n=4 -> [2, 2.25, 2.37, 2.44]
 # Сумма 9.06
+try:
+    n = int(input('Input positive int n: '))
+    if n < 0:
+        print('Uncorrect input - negative')
+    else:
+        my_list = []
+        sum = 0
+        for i in range(1, n + 1):
+            my_list.append(round((1 + 1/i)**i,2))
+            sum = sum + my_list[i-1]
+        print(f'Для n = {n} -> {my_list}')
+        print(f'Сумма {sum}')
+except:
+        print('Uncorrect input (float, symbol and ect.)')
