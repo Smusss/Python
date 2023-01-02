0469d4b4ec1beeab9308fbe8e1fd755a2e264c15
Smusss <lilly_dallas@bk.ru>
2023-01-02 16:53:41 +0300

    modified:   HomeWork_001/Task002_HW.py

--- HomeWork_001/Task002_HW.py
+++ HomeWork_001/Task002_HW.py
@@ -1,7 +1,15 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = int(input('Введите число x: '))
-y = int(input('Введите число y: '))
-z = int(input('Введите число z: '))
+#x = int(input('Введите число x: '))
+#y = int(input('Введите число y: '))
+#z = int(input('Введите число z: '))
 
-print(-(x or y or z)==(-x or -y or -z))
+trigger = True
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            if not(x or y or z) != (not x and not y and not z):
+                print (f'При x = {x}, y = {y}, z = {z} выражение ложно')
+                trigger = False
+if trigger:
+    print('Выражение всегда истинно')
