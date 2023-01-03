6c683c2724be6263c295be05e684ee8a7e5ddf4a
Smusss <lilly_dallas@bk.ru>
2023-01-03 16:25:25 +0300

    Add  HomeWork_003 Task002

---
+++ Homework_003/Task_002_HW.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример: [2, 3, 4, 5, 6] => [12, 15, 16];  [2, 3, 5, 6] => [12, 15]
+import random
+my_list = []
+for i in range(random.randint(1,10)):
+    my_list.append(random.randint(0,10))
+if len(my_list) % 2 == 0:
+    new_len = int(len(my_list) / 2)
+else:
+    new_len = int(len(my_list) / 2 + 1)
+new_list = []
+for i in range(new_len):
+    new_list.append(my_list[i] * my_list[len(my_list)- 1 - i])
+print(f'{my_list} => {new_list}')
