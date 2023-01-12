2308af9934aef07aa6682479aca7beddea94145d
Smusss <lilly_dallas@bk.ru>
2023-01-12 14:29:37 +0300

    Modified: Homework_002/Task003_HW.py

--- Homework_002/Task003_HW.py
+++ Homework_002/Task003_HW.py
@@ -25,3 +25,21 @@ for i in range(len(start_list)):
     start_list[i] = start_list[x]
     start_list[x] = temp
 print(f'Shuffle list: {start_list}')
+
+#не в стиле C)))
+import random
+def create_list():
+    my_list = []
+    count = random.randint(5, 20)                       # задаем случаейное количество элементов списка от 5 до 20
+    for i in range(count):                              # заполняем список случайными числами от 0 до 99
+        my_list.append(random.randint(0,99))
+    print(f'Start   list: {my_list}')
+    return my_list                # вывод первоначального списка
+def my_shuffle (my_list: list):
+    ni = random.randint(0, len(my_list)-1)
+    for i in range(len(my_list)):
+        my_list[i], my_list[ni] = my_list[ni], my_list[i]
+    print(f'Shuffle list: {my_list}')
+    return my_list
+my_list = create_list()
+shuffled = my_shuffle(my_list)
