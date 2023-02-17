d172214f67e917c6c346d0572b527cd980ed5881
Smusss <lilly_dallas@bk.ru>
2023-02-17 21:35:44 +0300

    version by global

--- Practice_009/heandlers.py
+++ Practice_009/heandlers.py
@@ -3,6 +3,12 @@ from aiogram import types
 import emoji
 import random
 
+game = False
+sweets_bank = 0
+max_take = 0
+turn = 0
+check = False
+
 win = 'YOU WIN!!! All sweets are yours!\n'
 lose = "YOU LOSE...I hope you don't like sweets.\n"
 
@@ -11,26 +17,28 @@ async def mes_start(message:types.Message):
     print(message, emoji.emojize(':thumbs_up:', language = 'alias'))
     await message.answer("Hello! Let's play in my favourite game!\n"
                         "Press:\n"
-                        "*/YES* for play\n"
-                        "*/RULES* for show game rules\n"
-                        "*/NO* if you are boring and busy person\n")
+                        "/YES for play\n"
+                        "/RULES for show game rules\n"
+                        "/NO if you are boring and busy person\n")
 
 @dp.message_handler(commands=['RULES'])
 async def mes_rules(message:types.Message):
-    await message.answer(f'OOkkeeeyy!\n'
-                         '*Rules of the game:*\n'
+    await message.answer(f'OOkkeeeyy!\n\n'
+                         'Rules of the game:\n'
                          'On the table is the number of sweets that we will choose at the start.\n'
                          'Two players play by making moves one after the other.\n'
                          'The first move is determined by a draw.\n'
                          'In one move, you can collect no more than the maximum amount that we chose at the beginning.\n'
                          'Whoever makes the last move wins and takes all the candies.\n\n'
-                         "Let's play, */YES*?")
+                         "Let's play, /YES?")
 
 @dp.message_handler(commands=['YES'])
 async def mes_yes(message:types.Message):
+    global game
+    global turn
     await message.answer("Let's get ready to rumble!\n"
-                         "How many sweets are in the game?"
-                         "Input '/set_bank' and count of sweets ")
+                         "How many sweets are in the game?\n"
+                         "Input /bank and a count of sweets ")
     game = True
     turn = random.randint(0, 1)
     return game, turn
@@ -41,16 +49,18 @@ async def mes_yes(message:types.Message):
     game = False
     return game
 
-@dp.message_handler(commands=['set_bank'])
+@dp.message_handler(commands=['bank'])
 async def mes_set(message:types.Message):
+    global sweets_bank
     count = message.text.split()[1]
     sweets_bank = int(count)
     await message.answer(f'Sweets bank = {sweets_bank}.\n'
-                          "Input '/set_max' and count of max count of sweets that can be taken by turn.")
+                          "Input /max and count of max a count of sweets that can be taken by turn.")
     return sweets_bank
 
-@dp.message_handler(commands=['set_max'])
+@dp.message_handler(commands=['max'])
 async def mes_set(message:types.Message):
+    global max_take
     count = message.text.split()[1]
     max_take = int(count)
     await message.answer(f'So, {max_take} sweets can be taken by turn.\n'
@@ -58,16 +68,20 @@ async def mes_set(message:types.Message):
     return max_take
 
 @dp.message_handler(commands=['GO'])
-async def mes_go(message:types.Message, game, sweets_bank, max_take, turn):
+async def mes_go(message:types.Message):
+    global game
+    global sweets_bank
+    global max_take
+    global turn
     if game is False:
-        await bot.send_message(message.from_user.id, 'But you said that you do not want to play. \n*Tell me /YES, Bro))* ')
+        await bot.send_message(message.from_user.id, 'But you said that you do not want to play. \nTell me /YES, Bro)) ')
     elif sweets_bank <= 0:
-        await bot.send_message(message.from_user.id, 'You did not set the count of sweets (sweets bank). \n*Tell me /set_bank and count!*')
+        await bot.send_message(message.from_user.id, 'You did not set the count of sweets (sweets bank). \nTell me /set_bank and count!')
     elif max_take <= 1:
-        await bot.send_message(message.from_user.id, 'You did not set the count of sweets can be taken by turn (max take). \n*Tell me /set_max and count!*')
+        await bot.send_message(message.from_user.id, 'You did not set the count of sweets can be taken by turn (max take). \nTell me /set_max and count!')
     else:
         if turn == 1:
-            await bot.send_message(message.from_user.id, f'Your turn. There are {sweets_bank} on the table. \n*How many sweets will you take?*')
+            await bot.send_message(message.from_user.id, f'Your turn. There are {sweets_bank} sweets on the table. \nHow many sweets will you take?')
         else:
             take = sweets_bank % (max_take + 1)
             if sweets_bank <= max_take:
@@ -75,17 +89,20 @@ async def mes_go(message:types.Message, game, sweets_bank, max_take, turn):
             elif take == 0:
                 take = 1
                 turn = 1
-                sweets_bank = sweets_bank - take
+            sweets_bank = sweets_bank - take
             await bot.send_message(message.from_user.id, f'The Bot turn. He took {take} sweets.\n'
                                   f'There are {sweets_bank} on the table now.\n'
                                     'How many sweets will you take?')
-            check = True
+        check = True
     return sweets_bank, turn, check
             
 @dp.message_handler()
-async def mes_all(message:types.Message, check, sweets_bank, max_take):
+async def mes_all(message:types.Message):
     global win
     global lose
+    global check
+    global sweets_bank
+    global max_take
     if check == True:
         if sweets_bank > 0:
             if message.text.isdigit():
@@ -109,13 +126,13 @@ async def mes_all(message:types.Message, check, sweets_bank, max_take):
                     mess = judge(turn)
                     bot.send_message(message.from_user.id,mess)
             else:
-                await bot.send_message(message.from_user.id, f'Input number!')
+                await bot.send_message(message.from_user.id, f'Input number.')
         else:
             mess = judge(turn)
             bot.send_message(message.from_user.id,mess)
     else:
         await message.answer(f"We cant't start the game(((\n"
-                             'Press: */YES* and follow the line! I am waiting for you)))')
+                             'Press: */YES* and follow the line. I am waiting for you)))')
 
     def check_max_and_over(max, bank, mess):
         count = int(mess)
@@ -135,10 +152,6 @@ async def mes_all(message:types.Message, check, sweets_bank, max_take):
         else:
             result = "YOU LOSE...I hope you don't like sweets.\n"
         return result
-
-
-
-
 
 @dp.message_handler(commands=['OOP'])
 async def mes_oop(message:types.Message):
