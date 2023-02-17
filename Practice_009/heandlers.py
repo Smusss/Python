26356c58ea3eac9aa540e4840dd5a2bd18f71e88
Smusss <lilly_dallas@bk.ru>
2023-02-17 20:40:35 +0300

    modified heandlers

--- Practice_009/heandlers.py
+++ Practice_009/heandlers.py
@@ -3,6 +3,9 @@ from aiogram import types
 import emoji
 import random
 
+win = 'YOU WIN!!! All sweets are yours!\n'
+lose = "YOU LOSE...I hope you don't like sweets.\n"
+
 @dp.message_handler(commands=['start'])
 async def mes_start(message:types.Message):
     print(message, emoji.emojize(':thumbs_up:', language = 'alias'))
@@ -21,7 +24,7 @@ async def mes_rules(message:types.Message):
                          'The first move is determined by a draw.\n'
                          'In one move, you can collect no more than the maximum amount that we chose at the beginning.\n'
                          'Whoever makes the last move wins and takes all the candies.\n\n'
-                         "Let's play, *YES*?")
+                         "Let's play, */YES*?")
 
 @dp.message_handler(commands=['YES'])
 async def mes_yes(message:types.Message):
@@ -80,69 +83,58 @@ async def mes_go(message:types.Message, game, sweets_bank, max_take, turn):
     return sweets_bank, turn, check
             
 @dp.message_handler()
-async def mes_all(message:types.Message, check, sweets_bank, max_take, turn):
-    while sweets_bank > 0:
-        if turn == 1:
-        await bot.send_message(message.from_user.id, f'Your turn. There are {sweets_bank} on the table. \n*How many sweets will you take?*')
+async def mes_all(message:types.Message, check, sweets_bank, max_take):
+    global win
+    global lose
+    if check == True:
+        if sweets_bank > 0:
+            if message.text.isdigit():
+                take = check_max_and_over(max_take, sweets_bank, message.text)
+                sweets_bank -= take   
+                await bot.send_message(message.from_user.id, f'You took {take} sweets, '
+                                       f'there are {sweets_bank} sweets on the table now.')
+                turn = 0
+                if sweets_bank > 0:
+                    take = sweets_bank % (max_take + 1)
+                    if sweets_bank <= max_take:
+                        take = sweets_bank
+                    elif take == 0:
+                        take = 1
+                        turn = 1
+                    sweets_bank = sweets_bank - take
+                    await bot.send_message(message.from_user.id, f'The Bot turn. He took {take} sweets.\n'
+                                                                f'There are {sweets_bank} on the table now.\n'
+                                                                'How many sweets will you take?')
+                else:
+                    mess = judge(turn)
+                    bot.send_message(message.from_user.id,mess)
+            else:
+                await bot.send_message(message.from_user.id, f'Input number!')
         else:
-            take = sweets_bank % (max_take + 1)
-            if sweets_bank <= max_take:
-                take = sweets_bank
-            elif take == 0:
-                take = 1
-                turn = 1
-                sweets_bank = sweets_bank - take
-            await bot.send_message(message.from_user.id, f'The Bot turn. He took {take} sweets.\n'
-                                  f'There are {sweets_bank} on the table now.\n'
-                                    'How many sweets will you take?')
-        if message.text.isdigit():
-        total-= int(message.text)
-        await bot.send_message(message.from_user.id, f'конфет осталось {total}')
-
-                    while sweets_bank > 0:
-                        if turn == 1:                                       # man's turn
-                            message = 'Your turn to take: '
-                            take = check_max_and_over(max_take, sweets_bank, message)
-                            turn = 0
-                            sweets_bank = sweets_bank - take
-                        else:                                               # comp's turn
-                            take = sweets_bank % (max_take + 1)
-                            if sweets_bank <= max_take:
-                                take = sweets_bank
-                            elif take == 0:
-                                take = 1
-                            print(f'Your Imaginary friend took sweets: {take}.')
-                            turn = 1
-                            sweets_bank = sweets_bank - take
-                        print(f'There are {sweets_bank} sweets in the bank.\n')
-                    else:
-                        if turn == 0:
-                            print(f'YOU WIN!!! All sweets are yours!\n')
-                        else:
-                            print(f"YOU LOSE...I hope you don't like sweets.\n")
-
-
-
-
-
-
-
-
+            mess = judge(turn)
+            bot.send_message(message.from_user.id,mess)
     else:
-    
-        await message.answer(f'Wow, look what i have got - {message.text}')
+        await message.answer(f"We cant't start the game(((\n"
+                             'Press: */YES* and follow the line! I am waiting for you)))')
 
+    def check_max_and_over(max, bank, mess):
+        count = int(mess)
+        if count <= max and count <= bank:
+            return count
+        else:
+            if count > bank and count > max:
+                 bot.send_message(message.from_user.id,(f'Over bank and over max. Try again! Bank is {bank}, max is {max}'))
+            elif count <= max and count > bank:
+                bot.send_message(message.from_user.id,(f'Over bank. Try again! Bank is {bank}'))
+            else:
+                bot.send_message(message.from_user.id,(f'Over max. Try again! Max is {max}'))
 
-@dp.message_handler()
-async def mes_all(message:types.Message):
-    global total
-    if message.text.isdigit():
-        total-= int(message.text)
-        await bot.send_message(message.from_user.id, f'конфет осталось {total}')
-    else:
-        await message.answer(f'Wow, look what i have got - {message.text}')
-
-
+    def judge(turn):
+        if turn == 0:
+            result = 'YOU WIN!!! All sweets are yours!\n'
+        else:
+            result = "YOU LOSE...I hope you don't like sweets.\n"
+        return result
 
 
 
